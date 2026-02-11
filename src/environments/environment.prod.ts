77b6aa10522e0a225c2c926ef7872d23b3179a26
abhosale <bhosaleankita16@gmail.com>
2026-02-11 20:07:06 +0530

    feat: add environment configuration and integrate Chart.js

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  supabaseUrl: 'https://jzvxahnvcdqmhsmjiyza.supabase.co/rest/v1/',
+  supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp6dnhhaG52Y2RxbWhzbWppeXphIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjY5MjQ0MzIsImV4cCI6MjA4MjUwMDQzMn0.NrEZtp0e8954YUcMHStR213LGGYBovet9KBvno3Qsz0'
+};
