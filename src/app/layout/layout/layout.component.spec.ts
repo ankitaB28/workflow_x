0145671114cc73e3d69704288dc918aaed58dc5b
abhosale <bhosaleankita16@gmail.com>
2026-02-14 20:25:53 +0530

    test: include SidebarComponent in LayoutComponent tests

--- src/app/layout/layout/layout.component.spec.ts
+++ src/app/layout/layout/layout.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LayoutComponent } from './layout.component';
+import { SidebarComponent } from '../sidebar/sidebar.component';
 
 describe('LayoutComponent', () => {
   let component: LayoutComponent;
@@ -8,7 +9,7 @@ describe('LayoutComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ LayoutComponent ]
+      declarations: [ LayoutComponent, SidebarComponent ]
     })
     .compileComponents();
   });
