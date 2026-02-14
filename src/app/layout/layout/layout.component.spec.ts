148950152be1f3129384e8ad9caf0ad2c21f1d96
abhosale <bhosaleankita16@gmail.com>
2026-02-14 20:30:14 +0530

    test: add RouterTestingModule and SidebarService to LayoutComponent tests

--- src/app/layout/layout/layout.component.spec.ts
+++ src/app/layout/layout/layout.component.spec.ts
@@ -2,6 +2,8 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LayoutComponent } from './layout.component';
 import { SidebarComponent } from '../sidebar/sidebar.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { SidebarService } from 'src/app/core/services/sidebar.service';
 
 describe('LayoutComponent', () => {
   let component: LayoutComponent;
@@ -9,7 +11,9 @@ describe('LayoutComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ LayoutComponent, SidebarComponent ]
+      declarations: [ LayoutComponent, SidebarComponent ],
+      imports: [RouterTestingModule],
+      providers: [SidebarService]
     })
     .compileComponents();
   });
